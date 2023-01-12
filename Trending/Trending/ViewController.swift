3f1fada947e9022d5f7ef3d918d49a8d5ee1e164
Emre AYDIN <ea@ea.tc>
2023-01-12 21:31:27 +0300

    Project created..

---
+++ Trending/Trending/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Trending
+//
+//  Created by Emre on 12.01.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
